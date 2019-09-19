992b009b462c995076d968e6017fed23baa5f2b0
“bao17634” <1763417523@qq.com>
2019-09-19 09:21:04 +0800

    修改文件路径

--- src/main/java/com/byr/warehouse/controller/UserController.java
+++ src/main/java/com/byr/warehouse/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -48,8 +49,6 @@ public class UserController {
     private CommonRepository<User> commonRepository;
 
 
-
-
     @RequestMapping(value = "/user-dologin", method = {RequestMethod.GET, RequestMethod.POST})
     public String UserLogin(User user, ModelMap modelMap, HttpServletRequest request) throws Exception {
         String page = "index";
@@ -73,10 +72,10 @@ public class UserController {
         int enterSize = indexService.getYestdayApplyEnterCount();
         int outSize = indexService.getYestdayApplyOutCount();
         int entrepotSize = indexService.getEntrpotSize();
-        modelMap.addAttribute("enterSize",enterSize);
-        modelMap.addAttribute("outSize",outSize);
-        modelMap.addAttribute("entrepotSize",entrepotSize);
-        logService.saveOpLog(user.getUsername(),"登录","成功",user.toString());
+        modelMap.addAttribute("enterSize", enterSize);
+        modelMap.addAttribute("outSize", outSize);
+        modelMap.addAttribute("entrepotSize", entrepotSize);
+        logService.saveOpLog(user.getUsername(), "登录", "成功", user.toString());
         return page;
     }
 
@@ -93,11 +92,10 @@ public class UserController {
     }
 
 
-
     @RequestMapping("/user-save")
     public String saveUser(User user) throws StoreException {
         List<User> userList = userRepository.findUserByusername(user.getUsername());
-        if(userList.size()>0){
+        if (userList.size() > 0) {
             throw new StoreException("该用户已存在，请重新选择不存在的用户名！");
         }
         try {
@@ -111,7 +109,6 @@ public class UserController {
     }
 
 
-
     @RequestMapping("/user-getAll")
     public String getAlllUser(User user, ModelMap modelMap, int pagenum) {
         String page = "user_list";
@@ -122,7 +119,7 @@ public class UserController {
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            sql.append( "1 = 1");
+            sql.append("1 = 1");
             int totalpage = jdbcTemplate.query(sql.toString(), new BeanPropertyRowMapper(User.class)).size();
             sql.append(" LIMIT " + (pagenum - 1) * pagesize + "," + pagesize);
             List<User> users = jdbcTemplate.query(sql.toString(), new BeanPropertyRowMapper(User.class));
@@ -132,38 +129,37 @@ public class UserController {
             modelMap.addAttribute("totalpage", PageUtil.getTotalPage(totalpage, pagesize));
         }
         return page;
-
     }
 
     @RequestMapping("/user-disabledUser")
     public String disabledUser(Long id, ModelMap modelMap, HttpSession session) {
-        User user1 = (User)session.getAttribute("user");
-        System.out.println("禁用账户"+id);
-        if(id==user1.getId()){
-            session.setAttribute("message","不能禁用自己");
+        User user1 = (User) session.getAttribute("user");
+        System.out.println("禁用账户" + id);
+        if (id == user1.getId()) {
+            session.setAttribute("message", "不能禁用自己");
             return "redirect:/user-getAll?pagenum=1";
         }
         User user = userRepository.findUserByid(id);
-        if(null!=user){
+        if (null != user) {
             user.setStatus(1);
             userRepository.save(user);
-            System.out.println("禁用账户"+user);
+            System.out.println("禁用账户" + user);
         }
         return "redirect:/user-getAll?pagenum=1";
     }
 
     @RequestMapping("/user-enableUser")
-    public String enableUser(Long id, ModelMap modelMa,HttpSession session) {
-        User user1 = (User)session.getAttribute("user");
-        if(id==(user1.getId())){
-            session.setAttribute("message","不能启用自己");
+    public String enableUser(Long id, ModelMap modelMa, HttpSession session) {
+        User user1 = (User) session.getAttribute("user");
+        if (id == (user1.getId())) {
+            session.setAttribute("message", "不能启用自己");
             return "redirect:/user-getAll?pagenum=1";
         }
         User user = userRepository.findUserByid(id);
-        if(null!=user){
+        if (null != user) {
             user.setStatus(2);
             userRepository.save(user);
-            System.out.println("启用账户"+user);
+            System.out.println("启用账户" + user);
         }
         return "redirect:/user-getAll?pagenum=1";
     }
@@ -174,13 +170,13 @@ public class UserController {
     }
 
     @RequestMapping("/user-delete")
-    public String deleteUser(Long id,HttpSession session) {
-        User user1 = (User)session.getAttribute("user");
-        if(id==(user1.getId())){
-            session.setAttribute("message","不能删除自己");
+    public String deleteUser(Long id, HttpSession session) {
+        User user1 = (User) session.getAttribute("user");
+        if (id == (user1.getId())) {
+            session.setAttribute("message", "不能删除自己");
             return "redirect:/user-getAll?pagenum=1";
         }
-        session.setAttribute("message","删除了用户"+userRepository.findUserByid(id).getUsername());
+        session.setAttribute("message", "删除了用户" + userRepository.findUserByid(id).getUsername());
         userRepository.deleteById(id);
         return "redirect:/user-getAll?pagenum=1";
     }
@@ -199,34 +195,34 @@ public class UserController {
         user.setPassword(EncrypeUtil.shaEncode(user.getPassword()));
         System.out.println(user);
         userRepository.save(user);
-        System.out.println("编辑用户"+user);
+        System.out.println("编辑用户" + user);
         return "redirect:/user-getAll?pagenum=1";
     }
 
     @RequestMapping("/user-dopwd")
-    public String upPwd(String oldPassword,String newPassword,String rePassword, ModelMap modelMap,HttpSession session) throws Exception {
-        logger.debug("修改密码"+oldPassword+"   "+rePassword+"  "+rePassword);
-        int i=0;
-        if(!newPassword.equals(rePassword)){
-            System.err.println("修改密码"+oldPassword+"   "+rePassword+"  "+rePassword);
+    public String upPwd(String oldPassword, String newPassword, String rePassword, ModelMap modelMap, HttpSession session) throws Exception {
+        logger.debug("修改密码" + oldPassword + "   " + rePassword + "  " + rePassword);
+        int i = 0;
+        if (!newPassword.equals(rePassword)) {
+            System.err.println("修改密码" + oldPassword + "   " + rePassword + "  " + rePassword);
             throw new StoreException("新密码与确认密码不同，请重新输入");
         }
-        User user = (User)session.getAttribute("user");
-        if(user==null) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
             throw new StoreException("用户会话过期，请重新登录");
         }
         List<User> userList = userRepository.findUserByusername(user.getUsername());
-        if(userList.size()==0){
+        if (userList.size() == 0) {
             throw new StoreException("该账户不存在！");
         }
-        System.err.println("查找到的用户"+userList);
-        for(User user1:userList){
-            if(user1.getPassword().equals(EncrypeUtil.shaEncode(oldPassword))){
+        System.err.println("查找到的用户" + userList);
+        for (User user1 : userList) {
+            if (user1.getPassword().equals(EncrypeUtil.shaEncode(oldPassword))) {
                 i++;
                 break;
             }
         }
-        if(i==0){
+        if (i == 0) {
             throw new StoreException("密码错误！");
         }
         user.setPassword(EncrypeUtil.shaEncode(newPassword));
@@ -237,21 +233,22 @@ public class UserController {
 
     /**
      * 推出系统
+     *
      * @param session
      * @return
      */
     @RequestMapping("/user-logout")
     public String logOut(HttpSession session) {
         //推出系统，注销seeesion,跳转到登陆页面
-        User user = (User)session.getAttribute("user");
-        if(user!=null) {
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
             session.removeAttribute("user");
         }
         return "login";
     }
 
     @RequestMapping("/mail")
-    public void sendMail(){
+    public void sendMail() {
         reportShedule.sendMail();
     }
 
--- src/main/java/com/byr/warehouse/dao/CommonRepository.java
+++ src/main/java/com/byr/warehouse/dao/CommonRepository.java
@@ -10,36 +10,33 @@ import java.lang.reflect.Field;
  */
 @Repository
 public class CommonRepository<T> {
-    public StringBuffer getFiledValues(T t,int pagenum) throws IllegalAccessException {
-        String table=t.getClass().toString().substring(t.getClass().toString().lastIndexOf(".")+1);
-        StringBuffer stringBuffer=new StringBuffer("SELECT * FROM "+table+" WHERE ");
-        StringBuffer sql=new StringBuffer();
-        if(t!=null)
-        {
-            Class tClass=  t.getClass();
-            Field[] fields =  tClass.getDeclaredFields();
-            int count=0;
-            for (int i=0;i<fields.length;i++)
-            {
-                String buf="";
+    public StringBuffer getFiledValues(T t, int pagenum) throws IllegalAccessException {
+        String table = t.getClass().toString().substring(t.getClass().toString().lastIndexOf(".") + 1);
+        StringBuffer stringBuffer = new StringBuffer("SELECT * FROM " + table + " WHERE ");
+        StringBuffer sql = new StringBuffer();
+        if (t != null) {
+            Class tClass = t.getClass();
+            Field[] fields = tClass.getDeclaredFields();
+            int count = 0;
+            for (int i = 0; i < fields.length; i++) {
+                String buf = "";
                 fields[i].setAccessible(true);
-                if(fields[i].get(t)!=null)
-                {
-                    if(fields[i].getType().toString().equals("int")&&fields[i].getInt(t)!=0) {
-                        if(count==0) {
-                            buf = fields[i].getName() + " LIKE "+"'%"+fields[i].get(t)+"%'";
-                        }else {
-                            buf +=" AND ";
-                            buf += fields[i].getName() + " LIKE "+"'%"+fields[i].get(t)+"%'";
+                if (fields[i].get(t) != null) {
+                    if (fields[i].getType().toString().equals("int") && fields[i].getInt(t) != 0) {
+                        if (count == 0) {
+                            buf = fields[i].getName() + " LIKE " + "'%" + fields[i].get(t) + "%'";
+                        } else {
+                            buf += " AND ";
+                            buf += fields[i].getName() + " LIKE " + "'%" + fields[i].get(t) + "%'";
                         }
                         sql.append(buf);
                         count++;
-                    }else if(fields[i].getType().toString().equals("class java.lang.String")){
-                        if(count==0) {
-                            buf = fields[i].getName() + " LIKE "+"'%"+fields[i].get(t)+"%'";
-                        }else {
-                            buf +=" AND ";
-                            buf += fields[i].getName() + " LIKE "+"'%"+fields[i].get(t)+"%'";
+                    } else if (fields[i].getType().toString().equals("class java.lang.String")) {
+                        if (count == 0) {
+                            buf = fields[i].getName() + " LIKE " + "'%" + fields[i].get(t) + "%'";
+                        } else {
+                            buf += " AND ";
+                            buf += fields[i].getName() + " LIKE " + "'%" + fields[i].get(t) + "%'";
                         }
                         sql.append(buf);
                         count++;
@@ -47,20 +44,19 @@ public class CommonRepository<T> {
                 }
             }
             stringBuffer.append(sql);
-        }else {
+        } else {
             return new StringBuffer("");
         }
         return stringBuffer;
     }
 
-    public static void main(String args[])
-    {
-        CommonRepository<ApplyEnter> commonRepository=new CommonRepository();
-        ApplyEnter applyEnter=new ApplyEnter();
+    public static void main(String args[]) {
+        CommonRepository<ApplyEnter> commonRepository = new CommonRepository();
+        ApplyEnter applyEnter = new ApplyEnter();
         applyEnter.setDataCode("sss");
         applyEnter.setApplyPersonId("22");
         try {
-            System.out.println(commonRepository.getFiledValues(applyEnter,2));
+            System.out.println(commonRepository.getFiledValues(applyEnter, 2));
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
